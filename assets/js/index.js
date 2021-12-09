97a7dd53d80436bf0392be63c2a4f970ff6d2617
DevGuyAhnaf <57180217+ahnaf-zamil@users.noreply.github.com>
2021-12-09 09:23:29 +0600

    Prototype finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(() => {
+  $("#copyright-text").html(
+    `Copyright &copy; ${new Date().getFullYear()} ACPS IT Club | All Rights Reserved`
+  );
+
   AOS.init();
 
   const options = {
@@ -9,4 +13,15 @@ $(document).ready(() => {
   };
 
   new Typed("#welcome", options);
+
+  myearth = new Earth("earth", {
+    location: { lat: 20, lng: 20 },
+    light: "none",
+    mapImage: "assets/img/hologram-map.svg",
+    transparent: true,
+    autoRotate: true,
+    autoRotateSpeed: 1.4,
+    autoRotateDelay: 0,
+    autoRotateStart: 0,
+  });
 });
